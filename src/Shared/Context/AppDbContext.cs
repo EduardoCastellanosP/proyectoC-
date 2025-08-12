88da084063486a31b8fdb872240609d0e4731432
eduardoeliascastellanospicon <eduardoeliascastellanospicon@gmail.com>
2025-08-12 11:16:35 -0500

    feat: :tada: Estructura inicial del proyecto

---
+++ src/Shared/Context/AppDbContext.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+using proyectc_.src.Shared.Entities;
+using proyectc_.src.Shared.Entities.Filtros;
+using proyectc_.src.Shared.Entities.Variedades;
+
+
+namespace proyectc_.src.Shared.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Usuario> Usuarios => Set<Usuario>();
+
+    public DbSet<Filtro> Filtros => Set<Filtro>();
+
+    public DbSet<Variedad> Variedades => Set<Variedad>();
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+
+
+
+
+
+
+}
