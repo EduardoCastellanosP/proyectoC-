d569532262e707eba64fabc58997e47dc2b0655e
julianap <194113895+JulianaPallares16@users.noreply.github.com.>
2025-08-12 11:37:06 -0500

    feat: :sparkles: Add interfaces de usuario
    interfaces de usuario

--- obj/Debug/net9.0/proyectc#.AssemblyInfo.cs
+++ obj/Debug/net9.0/proyectc#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectc#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2591b74c2dc3ac042280806aa58d5578fae57a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+031a0d95598dbfd0f6479c1e16706282c55cc31f")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectc#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectc#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ src/Modules/Usuarios/Application/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using proyectc_.src.Modules.Usuarios.Domain.Entities;
+
+namespace proyectc_.src.Modules.Usuarios.Application.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        Task<IEnumerable<Usuario>> GetAllAsync();
+        Task<Usuario?> GetByIdAsync(int id);
+        void Add(Usuario entity);
+        Task SaveAsync();
+    }
+}
